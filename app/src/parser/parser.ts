8ee738f00388241ecd8190e50e91dce37d6cf1e0
custompro98 <mitchjoa@gmail.com>
2022-06-07 09:04:46 -0400

    Use es2019's flat to flatten dice output

--- app/src/parser/parser.ts
+++ app/src/parser/parser.ts
@@ -1,72 +1,65 @@
-import { StraightValue } from '../modifiers'
-import { Summable } from '../summable'
-import Die, * as dice from '../dice'
+import { StraightValue } from "../modifiers";
+import { Summable } from "../summable";
+import Die, * as dice from "../dice";
 
 /*
-* This will match the pattern START, 0 or more numbers, the letter d, and 0 or more numbers, END
-* i.e. example matches include
-* d6 2d6 d4 10d4 1000d20 1d12
-* */
-const DICE_SHORTHAND_REGEX = new RegExp('^\\d*d\\d+$')
+ * This will match the pattern START, 0 or more numbers, the letter d, and 0 or more numbers, END
+ * i.e. example matches include
+ * d6 2d6 d4 10d4 1000d20 1d12
+ * */
+const DICE_SHORTHAND_REGEX = new RegExp("^\\d*d\\d+$");
 
 const parse = (input: string): Summable[] => {
-  const summables: Summable[] = []
+  const summables: Summable[] = [];
 
-  const diceInputs: string[] = []
-  const straightValueInputs: number[] = []
+  const diceInputs: string[] = [];
+  const straightValueInputs: number[] = [];
 
   input
-    .split('+')
-    .map(s => s.trim())
-    .map(s => {
-      const parsedNumber = parseInt(s, 10)
+    .split("+")
+    .map((s) => s.trim())
+    .map((s) => {
+      const parsedNumber = parseInt(s, 10);
 
       if (Number.isNaN(parsedNumber) || s !== parsedNumber.toString()) {
-        diceInputs.push(s)
+        diceInputs.push(s);
       } else {
-        straightValueInputs.push(parsedNumber)
+        straightValueInputs.push(parsedNumber);
       }
-    })
+    });
 
   diceInputs
-    .filter(s => {
-      const passes = DICE_SHORTHAND_REGEX.test(s)
+    .filter((s) => {
+      const passes = DICE_SHORTHAND_REGEX.test(s);
 
-      return passes
+      return passes;
     })
-    .map(s => {
-      const split = s.split('d')
-      const tuple: [number, string] = [
-        parseInt(split[0]) || 1,
-        `d${split[1]}`
-      ]
+    .map((s) => {
+      const split = s.split("d");
+      const tuple: [number, string] = [parseInt(split[0]) || 1, `d${split[1]}`];
 
-      return tuple
+      return tuple;
     })
     .filter(([_, s]) => Object.keys(dice.Dice).includes(s))
     .map(([n, s]) => {
-      const collected: Die[] = []
-      const idx: dice.Dice = s as dice.Dice
-      const roller: dice.RollFunction = dice[idx]
+      const collected: Die[] = [];
+      const idx: dice.Dice = s as dice.Dice;
+      const roller: dice.RollFunction = dice[idx];
 
       for (let i = 0; i < n; i++) {
-        collected.push(new Die(roller))
+        collected.push(new Die(roller));
       }
 
-      return collected
+      return collected;
     })
-    .map(darr => {
-      darr.forEach(d => {
-        summables.push(d)
-      })
-    })
+    .flat()
+    .map((d) => summables.push(d));
 
   straightValueInputs
-    .map(n => new StraightValue(n))
-    .forEach(sv => summables.push(sv))
+    .map((n) => new StraightValue(n))
+    .forEach((sv) => summables.push(sv));
 
+  return summables;
+};
 
-  return summables
-}
-
-export { parse }
+export { parse };
