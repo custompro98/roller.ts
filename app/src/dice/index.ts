e977d783d8cfbea3b7c68be970815f838ae154da
custompro98 <mitchjoa@gmail.com>
2022-07-03 16:03:48 -0400

    Export more useful things

--- app/src/dice/index.ts
+++ app/src/dice/index.ts
@@ -1,5 +1,6 @@
 import Die from "./dice";
 
-export { AceConfig, AceMap, AceOperator, Dice, RollFunction } from "./types";
+export type { AceConfig, DieConfig, RollFunction } from "./types";
+export { AceMap, AceOperator, Dice } from "./types"
 export { d2, d4, d6, d8, d10, d12, d20, d100, isValidDieIndex } from "./dice";
 export default Die;
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,2 +1,12 @@
+import Die from './dice'
+
 export { parse } from './parser'
-export { Summable } from './summable'
+
+export type { Summable } from './summable'
+
+export type { Modifier } from './modifiers'
+export { StraightValue } from './modifiers'
+
+export { Die }
+export { d2, d4, d6, d8, d10, d12, d20 } from './dice'
+export type { AceConfig, DieConfig, RollFunction } from './dice'
