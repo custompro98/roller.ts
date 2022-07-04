7d6a554f0b82fceaa2384ea2f88fada83ac56b9b
custompro98 <mitchjoa@gmail.com>
2022-07-04 14:59:36 -0400

    Workflow to auto-compile for deno

---
+++ app/tools/push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+git config user.name custompro98-ci
+git config user.email mitchjoa@gmail.com
+git add . -f
+
+if ! git diff --cached --exit-code > /dev/null;
+then
+  git commit -m "Build from GitHub Action $GITHUB_SHA"
+  git push
+else
+  echo 'No changes to release'
+fi
