64edafeb14777dbf91c950200d6ae8b38b5399c8
custompro98 <mitchjoa@gmail.com>
2023-01-04 11:52:30 -0500

    Deno compile

--- app/deno/_src/dice/index.ts
+++ app/deno/_src/dice/index.ts
@@ -1,6 +1,21 @@
 import Die from "./dice.ts";
 
 export type { AceConfig, DieConfig, RollFunction } from "./types.ts";
-export { AceMap, AceOperator, Dice } from "./types.ts"
-export { d2, d4, d6, d8, d10, d12, d20, d100, isValidDieIndex } from "./dice.ts";
+export { AceMap, AceOperator, Dice } from "./types.ts";
+export {
+    d2,
+    d3,
+    d4,
+    d5,
+    d6,
+    d8,
+    d10,
+    d12,
+    d16,
+    d20,
+    d24,
+    d30,
+    d100,
+    isValidDieIndex,
+} from "./dice.ts";
 export default Die;
