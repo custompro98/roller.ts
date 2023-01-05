a78ae301d6eeb73ce27426c726ddd2ba22cd1a88
custompro98 <mitchjoa@gmail.com>
2023-01-04 21:15:30 -0500

    Add the rest of the dcc dice

--- app/src/dice/dice.ts
+++ app/src/dice/dice.ts
@@ -1,8 +1,8 @@
-import type { Summable } from "../summable";
+import type { Summable } from "../summable"
 
-import type { AceConfig, DieConfig, RollFunction } from "./types";
+import type { AceConfig, DieConfig, RollFunction } from "./types"
 import { AceOperator } from "./types"
-import { roller } from "./roller";
+import { roller } from "./roller"
 
 const dice = {
   d2: roller(2),
@@ -10,96 +10,97 @@ const dice = {
   d4: roller(4),
   d5: roller(5),
   d6: roller(6),
+  d7: roller(6),
   d8: roller(8),
   d10: roller(10),
   d12: roller(12),
+  d14: roller(14),
   d16: roller(16),
   d20: roller(20),
   d24: roller(24),
   d30: roller(30),
   d100: roller(100),
-};
+}
 
-export const { d2, d3, d4, d5, d6, d8, d10, d12, d16, d20, d24, d30, d100 } = dice;
+export const { d2, d3, d4, d5, d6, d7, d8, d10, d12, d14, d16, d20, d24, d30, d100 } = dice
 
-export const isValidDieIndex = (s: string): boolean =>
-  Object.keys(dice).includes(s);
+export const isValidDieIndex = (s: string): boolean => Object.keys(dice).includes(s)
 
 export default class Die implements Summable {
-  private DEFAULT_DROPPED = 0;
+  private DEFAULT_DROPPED = 0
 
-  private _roll: RollFunction;
-  private _nDice: number;
-  private _dropHighest: number;
-  private _dropLowest: number;
-  private _explodesOn: AceConfig;
+  private _roll: RollFunction
+  private _nDice: number
+  private _dropHighest: number
+  private _dropLowest: number
+  private _explodesOn: AceConfig
 
-  private _results: number[];
+  private _results: number[]
 
   constructor(
     roller: RollFunction,
-    nDice: number = 1,
+    nDice = 1,
     { dh, dl, ace }: DieConfig = {
       dh: 0,
       dl: 0,
       ace: { target: 0, operator: AceOperator.eq },
-    }
+    },
   ) {
-    this._roll = roller;
-    this._nDice = nDice;
-    this._dropHighest = dh || this.DEFAULT_DROPPED;
-    this._dropLowest = dl || this.DEFAULT_DROPPED;
-    this._explodesOn = ace || { target: 0, operator: AceOperator.eq };
+    this._roll = roller
+    this._nDice = nDice
+    this._dropHighest = dh || this.DEFAULT_DROPPED
+    this._dropLowest = dl || this.DEFAULT_DROPPED
+    this._explodesOn = ace || { target: 0, operator: AceOperator.eq }
 
-    this._results = [];
-    this.roll();
+    this._results = []
+    this.roll()
   }
 
   value(): number {
-    let list = this._results;
-    let numDropped = 0;
+    let list = this._results
+    let numDropped = 0
 
     if (this._dropLowest > 0) {
-      numDropped = this._dropLowest;
-      list = list.reverse().slice(0, this._nDice - numDropped);
+      numDropped = this._dropLowest
+      list = list.reverse().slice(0, this._nDice - numDropped)
     } else if (this._dropHighest > 0) {
-      numDropped = this._dropHighest;
-      list = list.slice(0, this._nDice - numDropped);
+      numDropped = this._dropHighest
+      list = list.slice(0, this._nDice - numDropped)
     }
 
-    return list.reduce((acc, cur) => acc + cur, 0);
+    return list.reduce((acc, cur) => acc + cur, 0)
   }
 
   reroll(): void {
-    this._results = [];
-    this.roll();
+    this._results = []
+    this.roll()
   }
 
   private roll(): void {
     for (let i = 0; i < this._nDice; i++) {
-      let val: number;
+      let val: number
       do {
-        val = this._roll();
-        this._results.push(val);
-      } while (this.didExplode(val));
+        val = this._roll()
+        this._results.push(val)
+      } while (this.didExplode(val))
     }
 
-    this._results.sort();
+    this._results.sort()
   }
 
   private didExplode(n: number): boolean {
     switch (this._explodesOn.operator) {
       case AceOperator.eq: {
-        return n === this._explodesOn.target;
+        return n === this._explodesOn.target
       }
       case AceOperator.ge: {
-        return n >= this._explodesOn.target;
+        return n >= this._explodesOn.target
       }
       case AceOperator.le: {
-        return n <= this._explodesOn.target;
+        return n <= this._explodesOn.target
       }
       default: {
-        return false;
+        return false
       }
     }
   }
